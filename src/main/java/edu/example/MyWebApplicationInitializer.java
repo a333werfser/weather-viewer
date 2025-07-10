d59093d9ece507312a0e9a5e0e810df376938a46
usxr <oxxxydn@gmail.com>
2025-07-10 16:04:56 +0300

    initial

---
+++ src/main/java/edu/example/MyWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package edu.example;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(WebConfig.class);
+
+        var registration = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(applicationContext));
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
